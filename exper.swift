1b60049c4d06bdff0578d315e441145bbb6b364e
Petr Homola <homola.p@gmail.com>
2025-11-02 04:36:33 +0000

    +update exper

--- exper.swift
+++ exper.swift
@@ -8,7 +8,7 @@ struct Main {
             formatter.dateFormat = "d MMM y, HH:mm"
             guard let from = formatter.date(from: "1 Sep 2025, 01:00") else { throw RuntimeError.badDateTime }
             guard let to = formatter.date(from: "5 Sep 2025, 23:00") else { throw RuntimeError.badDateTime }
-            let (candles, meta) = try await fetchYahooData(symbol: "AAPL", from: from, to: to, interval: "1d")
+            let (candles, meta) = try await fetchYahooData(symbol: "AAPL", from: from, to: to, interval: .day1)
             print("\(meta.currency) \(meta.fullExchangeName)")
             for candle in candles {
                 print("\(candle.timestamp) \(candle.volume) \(candle.low) \(candle.open) \(candle.close) \(candle.high)")
