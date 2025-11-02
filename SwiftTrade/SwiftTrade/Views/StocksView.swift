dca9042d3287baa9064d5558233c9f143e6b5ab8
Petr Homola <homola.p@gmail.com>
2025-11-02 11:44:29 +0000

    symbols

--- SwiftTrade/SwiftTrade/Views/StocksView.swift
+++ SwiftTrade/SwiftTrade/Views/StocksView.swift
@@ -7,23 +7,31 @@
 
 import SwiftUI
 
+let allIntervals: [Interval] = [.hour1, .day1]
+let allSymbols: [Symbol] = [Symbol(text: "AAPL"), Symbol(text: "MSFT"), Symbol(text: "GOOGL")]
+
 struct StocksView: View {
+    @State private var symbol: String = allSymbols[0].id
     @State private var from: Date = .now
     @State private var to: Date = .now
     @State private var interval: Interval = .day1
-    let intervals: [Interval] = [.hour1, .day1]
+    @State private var isWorking = false
+    @State private var errorWrapper: ErrorWrapper?
 
     var body: some View {
         VStack {
+            //TextField("symbol", text: $symbol, prompt: Text("symbol"))
+            Picker("symbol", selection: $symbol) {
+                ForEach(allSymbols) { symbol in
+                    Text(symbol.text)
+                }
+            }
             DatePicker("from", selection: $from, displayedComponents: .date)
             DatePicker("to", selection: $to, displayedComponents: .date)
             Picker("interval", selection: $interval) {
-                ForEach(intervals) { interval in
-                    Text(interval.localizedStringKey).tag(interval)
+                ForEach(allIntervals) { interval in
+                    Text(interval.localizedStringKey)
                 }
-            }
-            .onChange(of: interval) {
-                print("\(interval) \(YahooInterval(interval))")
             }
             Button("chart") {
                 if let from = Calendar.current.date(from: Calendar.current.dateComponents([.year, .month, .day], from: from)) {
@@ -31,21 +39,27 @@ struct StocksView: View {
                         let to = to.addingTimeInterval(24 * 60 * 60 - 1)
                         Task {
                             do {
-                                let (candles, meta) = try await fetchYahooData(symbol: "AAPL", from: from, to: to, interval: interval)
+                                let (candles, meta) = try await fetchYahooData(symbol: symbol, from: from, to: to, interval: interval)
                                 print("\(meta.currency) \(meta.fullExchangeName)")
                                 for candle in candles {
                                     print("\(candle.timestamp) \(candle.volume) \(candle.low) \(candle.open) \(candle.close) \(candle.high)")
                                 }
+                                print(candles.count)
                             } catch {
                                 print("error: \(error.localizedDescription)")
+                                errorWrapper = ErrorWrapper(error: error, guidance: "Failed to fetch data.")
                             }
                         }
                     }
                 }
             }
             .buttonStyle(.borderedProminent)
+            .disabled(isWorking || symbol.isEmpty)
         }
         .padding()
+        .sheet(item: $errorWrapper) { wrapper in
+            ErrorView(errorWrapper: wrapper)
+        }
     }
 }
 
