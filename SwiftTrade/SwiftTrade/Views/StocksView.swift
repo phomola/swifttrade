4dd4edd6d2ed046de1ae9511e0a6dea53df32abe
Petr Homola <homola.p@gmail.com>
2025-11-02 06:25:25 +0000

    +interval

--- SwiftTrade/SwiftTrade/Views/StocksView.swift
+++ SwiftTrade/SwiftTrade/Views/StocksView.swift
@@ -10,11 +10,21 @@ import SwiftUI
 struct StocksView: View {
     @State private var from: Date = .now
     @State private var to: Date = .now
+    @State private var interval: Interval = .day1
+    let intervals: [Interval] = [.hour1, .day1]
 
     var body: some View {
         VStack {
             DatePicker("from", selection: $from, displayedComponents: .date)
             DatePicker("to", selection: $to, displayedComponents: .date)
+            Picker("interval", selection: $interval) {
+                ForEach(intervals) { interval in
+                    Text(verbatim: "\(interval)").tag(interval)
+                }
+            }
+            .onChange(of: interval) {
+                print("\(interval) \(YahooInterval(interval))")
+            }
             Button("chart") {
                 if let from = Calendar.current.date(from: Calendar.current.dateComponents([.year, .month, .day], from: from)) {
                     if let to = Calendar.current.date(from: Calendar.current.dateComponents([.year, .month, .day], from: to)) {
