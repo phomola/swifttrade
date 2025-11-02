27d717fd4fb085274f09fc64dd3deeca795c19ec
Petr Homola <homola.p@gmail.com>
2025-11-02 05:45:00 +0000

    initial version

--- SwiftTrade/SwiftTrade/Findata/YahooData.swift
+++ SwiftTrade/SwiftTrade/Findata/YahooData.swift
@@ -1,23 +1,11 @@
+//
+//  YahooData.swift
+//  SwiftTrade
+//
+//  Created by Petr Homola on 02/11/2025.
+//
+
 import Foundation
-
-enum RuntimeError: Error {
-    case badUrl
-    case badDateTime
-}
-
-protocol DataMeta {
-    var currency: String { get }
-    var fullExchangeName: String { get }
-}
-
-struct Candle {
-    let timestamp: Date
-    let volume: Float64
-    let open: Float64
-    let close: Float64
-    let low: Float64
-    let high: Float64
-}
 
 struct YahooResponse: Decodable {
     let chart: YahooChart
---
+++ SwiftTrade/SwiftTrade/Views/StocksView.swift
@@ -0,0 +1,18 @@
+//
+//  StocksView.swift
+//  SwiftTrade
+//
+//  Created by Petr Homola on 02/11/2025.
+//
+
+import SwiftUI
+
+struct StocksView: View {
+    var body: some View {
+        Text("stocks")
+    }
+}
+
+#Preview {
+    StocksView()
+}
