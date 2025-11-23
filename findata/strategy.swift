713e40ddea83806a1411d7f2a214d8f09263e8d0
Petr Homola <homola.p@gmail.com>
2025-11-23 02:19:51 +0000

    signals

--- findata/strategy.swift
+++ findata/strategy.swift
@@ -14,21 +14,48 @@ class Dataset: CustomStringConvertible {
     var description: String { data.description }
 }
 
-class Portfolio {
-    var assets: [String: Float64] = [:]
+class Signal {
+    var previousValue: Bool?
+    var currentValue: Bool?
+    
+    func set(value: Bool) {
+        previousValue = currentValue
+        currentValue = value
+    }
+
+    enum Change {
+        case up
+        case down
+        case unchanged
+        case undetermined
+    }
+
+    var change: Change {
+        if let previousValue = previousValue {
+            if currentValue! && !previousValue { return .up }
+            if !currentValue! && previousValue { return .down }
+            return .unchanged
+        }
+        return .undetermined
+    }
 }
 
 class Context {
-    let portfolio: Portfolio
+    var cash: Float64
+    var asset: Float64 = 0.0
+    var index: Int = 0
+    var value: Float64 = 0.0
     var indicators: [Indicator] = []
 
-    init(portfolio: Portfolio) {
-        self.portfolio = portfolio
+    init(cash: Float64) {
+        self.cash = cash
     }
 
     func add(indicator: Indicator) {
         indicators.append(indicator)
     }
+
+    // func buy(for amount: Float64) -> Bool {}
 }
 
 protocol Strategy {
@@ -57,14 +84,15 @@ class Backtester {
         var datasets: [String: Dataset]
     }
 
-    func run(strategy: Strategy) -> Result {
-        let portfolio = Portfolio()
-        let context = Context(portfolio: portfolio)
+    func run(strategy: Strategy, cash: Float64) -> Result {
+        let context = Context(cash: cash)
         strategy.setup(context: context)
-        for value in data {
+        for (index, value) in data.enumerated() {
             for indicator in context.indicators {
                 indicator.add(value: value)
             }
+            context.index = index
+            context.value = value
             strategy.loop(context: context)
         }  
         return Result(datasets: strategy.datasets)
