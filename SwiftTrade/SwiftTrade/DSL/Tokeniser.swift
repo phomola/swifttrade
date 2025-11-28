d6ab4f880af05ac1440d02a2c18f2a0e46071001
Petr Homola <homola.p@gmail.com>
2025-11-28 18:21:51 +0000

    tokeniser with position

--- SwiftTrade/SwiftTrade/DSL/Tokeniser.swift
+++ SwiftTrade/SwiftTrade/DSL/Tokeniser.swift
@@ -7,36 +7,47 @@
 
 import Foundation
 
-enum Token {
-    case identifier(String)
-    case number(Int)
-    case string(String)
-    case symbol(String)
-}
-
 func scannerExper() {
-    let scanner = Scanner(string: """
+    for token in tokenizer(string: """
         abcd
         1234
         "ABCD"
         efgh ijkl
-        """)
+        """) {
+        print(token)
+    }
+}
+
+struct Position {
+    let index: String.Index
+}
+
+enum Token {
+    case identifier(value: String, position: Position)
+    case number(value: Int, position: Position)
+    case string(value: String, position: Position)
+    case symbol(value: String, position: Position)
+}
+
+func tokenizer(string: String) -> [Token] {
+    let scanner = Scanner(string: string)
     scanner.charactersToBeSkipped = .whitespacesAndNewlines
     var tokens: [Token] = []
     while !scanner.isAtEnd {
+        let index = scanner.currentIndex
         if let text = scanner.scanCharacters(from: .decimalDigits) {
-            tokens.append(.number(Int(text)!))
+            tokens.append(.number(value: Int(text)!, position: Position(index: index)))
             continue
         }
         if let text = scanner.scanCharacters(from: .alphanumerics) {
-            tokens.append(.identifier(text))
+            tokens.append(.identifier(value: text, position: Position(index: index)))
             continue
         }
         let char = scanner.string[scanner.currentIndex]
         scanner.currentIndex = scanner.string.index(scanner.currentIndex, offsetBy: 1)
         if char == "\"" {
             if let text = scanner.scanUpToString("\"") {
-                tokens.append(.string(text))
+                tokens.append(.string(value: text, position: Position(index: index)))
                 scanner.currentIndex = scanner.string.index(scanner.currentIndex, offsetBy: 1)
                 continue
             }
@@ -44,12 +55,10 @@ func scannerExper() {
         if scanner.charactersToBeSkipped!.contains(char.unicodeScalars.first!) {
             continue
         }
-        tokens.append(.symbol(String(char)))
+        tokens.append(.symbol(value: String(char), position: Position(index: index)))
 //        if let text = scanner.scanUpToCharacters(from: .newlines) {
 //            print("line: <\(text)>")
 //        }
     }
-    for token in tokens {
-        print(token)
-    }
+    return tokens
 }
