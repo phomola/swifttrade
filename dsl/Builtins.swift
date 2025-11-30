db6b8f8df6046bf5fc974e8bf95e9b663a95314e
Petr Homola <homola.p@gmail.com>
2025-11-30 14:19:41 +0000

    list of built-in functions (initial)

---
+++ dsl/Builtins.swift
@@ -0,0 +1,5 @@
+// Built-in functions for the DLS:
+// - promptLlm: prompts the LLM (high-level)
+// - treeTransform: converts a strategy in the form of a tree into code
+// - embedding: returns the embedding of a prompt, strategy, algorithm or indicator
+// - queryVector: queries the database of strategies (via vectors)
