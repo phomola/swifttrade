8ae1b8180d868498333841fe9fd4819a40f56b0d
Petr Homola <homola.p@gmail.com>
2025-12-25 00:18:16 +0000

    value file

--- dsl/Eval.swift
+++ dsl/Eval.swift
@@ -75,23 +75,3 @@ final class StringExpression: Expression {
         return StringValue(value: value)
     }
 }
-
-protocol Value: CustomStringConvertible {}
-
-struct IntegerValue: Value {
-    let value: Int
-
-    var description: String { "\(value)" }
-}
-
-struct FloatValue: Value {
-    let value: Double
-
-    var description: String { "\(value)" }
-}
-
-struct StringValue: Value {
-    let value: String
-
-    var description: String { value }
-}
